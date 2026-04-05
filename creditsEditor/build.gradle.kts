b639d0de264caf5325d0f19d819e2d34785602c9
Léa Gris <lea.gris@noiraude.net>
2026-04-05 02:23:59 +0200

    build(creditsEditor): configure as runnable application with run alias

--- creditsEditor/build.gradle.kts
+++ creditsEditor/build.gradle.kts
@@ -11,10 +11,18 @@ val gsonVersion: String by gradle.extra
 
 dependencies {
     implementation(project(":libCredits"))
-    compileOnly("com.google.code.gson:gson:$gsonVersion")
+    implementation("com.google.code.gson:gson:$gsonVersion")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+application {
+    mainClass.set("net.noiraude.creditseditor.Main")
+}
+
+tasks.named<JavaExec>("run") {
+    args(rootProject.file("src/main/resources"))
 }
